87f14adf0973e3aca310d6f08a4764837f091bdb
AlejoBerg <alejo_B@outlook.com>
2021-02-11 00:14:16 -0300

    Board manager created + Board manager created * Now each time you press finish match button, board will be cleaned

---
+++ Proyecto Investigacion - UADE/Assets/Scripts/BoardScripts/BoardManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardManager : MonoBehaviour
+{
+    [SerializeField] private GameObject[] _boardLines;//TopLine and Button line 
+    [SerializeField] private GameObject _playerDeckOfCards;//TopLine and Button line 
+
+    private List<GameObject> _cardsPlayed;
+
+    public void OnFinishMatchButtonPressed()
+    {
+        _cardsPlayed = new List<GameObject>();
+
+        GetCardsPlayed();
+        ReturnCardsPlayedToDeck();
+    }
+
+    private List<GameObject> GetCardsPlayed()
+    {
+        foreach (var line in _boardLines) //Recorro los renglones del tablero, para acceder a sus hijos y agregarlos a "cardsPlayed"
+        {
+            if (line.transform.childCount > 0)
+            {
+                foreach (Transform t in line.transform)
+                {
+                    _cardsPlayed.Add(t.gameObject);
+                }
+            }
+        }
+        return _cardsPlayed;
+    }
+
+    private void ReturnCardsPlayedToDeck()
+    {
+        foreach (var card in _cardsPlayed)
+        {
+            card.transform.parent = _playerDeckOfCards.transform;
+        }
+    }
+}
