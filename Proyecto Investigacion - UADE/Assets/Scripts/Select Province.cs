8923214353e7309808ebcb42e9d8eb4ae972819e
RomanVitolo <68485569+RomanVitolo@users.noreply.github.com>
2021-02-11 20:07:39 -0300

    Puesta en escena de las provincias en forma individual

---
+++ Proyecto Investigacion - UADE/Assets/Scripts/Select Province.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectProvince : MonoBehaviour
+{
+        void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction, Mathf.Infinity);
+            if (hit)
+            {
+                Debug.Log(hit.collider.gameObject.name);
+            }
+        }
+    }
+}
+// https://gamedev.stackexchange.com/questions/117542/picking-a-2d-box-collider-with-the-mouse-in-unity  Referencia para seguir probando
