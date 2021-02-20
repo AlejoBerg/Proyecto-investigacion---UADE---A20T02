6e006ff7b15f8aa922fc5181f752304eedbf7a64
AlejoBerg <alejo_B@outlook.com>
2021-02-20 14:43:41 -0300

    Skybox visual effect created and added to AlejoScene

---
+++ Proyecto Investigacion - UADE/Assets/Scripts/Miscellaneous/SkyboxManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyboxManager : MonoBehaviour
+{
+    [SerializeField] private float _skyboxRotSpeed = 5f;
+    [SerializeField] private float _skyboxExposureSpeed = 5f;
+    [SerializeField] private float _skyboxMaxExposure = 0.4f;
+
+    void Start()
+    {
+
+    }
+
+    void LateUpdate()
+    {
+        RenderSettings.skybox.SetFloat("_Rotation", Time.time * _skyboxRotSpeed);
+
+        float lerp = Mathf.PingPong(Time.time / _skyboxExposureSpeed, _skyboxMaxExposure);
+        print(lerp);
+        RenderSettings.skybox.SetFloat("_Exposure", lerp);
+        //_skybox.material.SetFloat("_Rotation", Time.time * _skyboxRotSpeed);
+    }
+}
