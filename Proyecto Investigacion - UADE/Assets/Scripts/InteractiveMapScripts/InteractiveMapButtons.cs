7fb668bacf8f61ce821643aec815020e1ea38d1d
AlejoBerg <alejo_B@outlook.com>
2021-02-26 00:17:15 -0300

    Map working correctly - fix

--- Proyecto Investigacion - UADE/Assets/Scripts/InteractiveMapScripts/InteractiveMapButtons.cs
+++ Proyecto Investigacion - UADE/Assets/Scripts/InteractiveMapScripts/InteractiveMapButtons.cs
@@ -1,14 +1,17 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class InteractiveMapButtons : MonoBehaviour
 {
     [SerializeField] private GameObject _blurImageRef;
+    public event Action OnBackButtonPressed;
 
     public void OnBackFromProvince(InteractiveMapCameraController cameraRef)
     {
         cameraRef.ResetCameraValues();
         _blurImageRef.SetActive(false);
+        OnBackButtonPressed?.Invoke();
         //Ocultar boton de back, lo hago por editor
     }
 
--- Proyecto Investigacion - UADE/Assets/Scripts/InteractiveMapScripts/ProvinceBehaviour.cs
+++ Proyecto Investigacion - UADE/Assets/Scripts/InteractiveMapScripts/ProvinceBehaviour.cs
@@ -2,37 +2,72 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class ProvinceBehaviour : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
 {
     [SerializeField] private InteractiveMapCameraController _cameraControllerRef;
+
+    //Buttons Variables
     [SerializeField] private Vector3 _backButtonOffset;
     [SerializeField] private Vector3 _playButtonOffset;
     [SerializeField] private GameObject _backButtonRef;
     [SerializeField] private GameObject _playGameButtonRef;
     [SerializeField] private float _delayBeforeShowingButtons = 1;
+
+    //Image Effects Variables
     [SerializeField] private GameObject _blurImage;
-    
+
+    private Image _currentImage;
+    private Color _currentImageColor;
+    private InteractiveMapButtons _interactiveMapButtonRef;
+    private bool _isFocused = false;
+
+    private void Awake()
+    {
+        _interactiveMapButtonRef = _backButtonRef.GetComponent<InteractiveMapButtons>();
+        _interactiveMapButtonRef.OnBackButtonPressed += OnBackButtonPressedHandler;
+
+        _currentImage = this.GetComponentInChildren<Image>();
+
+        _currentImageColor = _currentImage.color;
+        _currentImageColor.a = 0;
+        _currentImage.color = _currentImageColor; // Ahora, cuando vuelvas lo que vas a tener que hacer es que el cambio de alfa lo haga cada vez que te paras encima y cuando salis
+    }
 
     public void OnPointerClick(PointerEventData eventData)
     {
-        print($"clickeo sobre {this.name}");
         _cameraControllerRef.AssignNewFieldViewAndTarget(10, this.transform, this.transform);
         StartCoroutine(AdjustButtons());
+
+        _currentImage.enabled = true;
 
         _blurImage.transform.SetAsLastSibling();
         _blurImage.SetActive(true);
         this.transform.SetAsLastSibling();
+
+        _isFocused = true;
+        _currentImageColor.a = 100;
+        _currentImage.color = _currentImageColor;
     }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-        //Glow
+        if (!_isFocused) 
+        {
+            _currentImageColor.a = 100;
+            _currentImage.color = _currentImageColor;
+        }
+        
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        //Sacar Glow
+        if (!_isFocused)
+        {
+            _currentImageColor.a = 0;
+            _currentImage.color = _currentImageColor;
+        }
     }
 
     IEnumerator AdjustButtons()
@@ -48,5 +83,14 @@ public class ProvinceBehaviour : MonoBehaviour, IPointerEnterHandler, IPointerEx
 
         _playGameButtonRef.transform.position = this.transform.position + _playButtonOffset;
         _playGameButtonRef.SetActive(true);
+    }
+
+    private void OnBackButtonPressedHandler()
+    {
+        _currentImageColor.a = 0;
+        _currentImage.color = _currentImageColor;
+
+        _blurImage.SetActive(false);
+        _isFocused = false;
     }
 }
