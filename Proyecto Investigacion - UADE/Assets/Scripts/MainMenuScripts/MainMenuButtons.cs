74cdfce5b725aeca88f3f7523d900600eb353333
AlejoBerg <alejo_B@outlook.com>
2021-03-01 19:48:47 -0300

    Main menu re created

---
+++ Proyecto Investigacion - UADE/Assets/Scripts/MainMenuScripts/MainMenuButtons.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuButtons : MonoBehaviour
+{
+    public void OnPlayGameButton()
+    {
+        SceneManager.LoadScene("StartScene");
+    }
+
+    public void OnExitGameButton()
+    {
+        print("Quitting Game");
+        Application.Quit();
+    }
+}
