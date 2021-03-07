face315aaaada5182ba70e83aaf3b6d0801aff00
AlejoBerg <alejo_B@outlook.com>
2021-03-07 03:46:03 -0300

    Pause menu and back button from highScore added

---
+++ Proyecto Investigacion - UADE/Assets/Scripts/MainMenuScripts/PauseMenuButtons.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenuButtons : MonoBehaviour
+{
+    [SerializeField] private ChangeScene _changeSceneRef;
+    private bool _isPaused = false;
+
+    private void Update()
+    {
+        CheckInput();
+    }
+
+    private void CheckInput()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) && !_isPaused)
+        {
+            _isPaused = true;
+            _changeSceneRef.OnChangeOneSceneAlpha(1);
+        }
+        else if (Input.GetKeyDown(KeyCode.Escape) && _isPaused)
+        {
+            _isPaused = false;
+            _changeSceneRef.OnChangeOneSceneAlpha(0);
+        }
+    }
+}
