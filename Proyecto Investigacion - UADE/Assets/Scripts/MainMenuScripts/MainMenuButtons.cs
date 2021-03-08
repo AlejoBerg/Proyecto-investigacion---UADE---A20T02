87f484caffc9a1c64c02b36019d66ad1c02097f1
AlejoBerg <alejo_B@outlook.com>
2021-03-08 12:55:12 -0300

    Cleaning script

--- Proyecto Investigacion - UADE/Assets/Scripts/MainMenuScripts/MainMenuButtons.cs
+++ Proyecto Investigacion - UADE/Assets/Scripts/MainMenuScripts/MainMenuButtons.cs
@@ -14,25 +14,4 @@ public class MainMenuButtons : MonoBehaviour
         print("Quitting Game");
         Application.Quit();
     }
-
-    //public void SetUsername(TMP_InputField inputFieldUsername)
-    //{
-    //    string username;
-
-    //    if (inputFieldUsername.text.Length == 0) { username = "Unknown"; }
-    //    else { username = inputFieldUsername.text; }
-
-    //    SetScoreboard(username);
-    //}
-
-    //private void SetScoreboard(string username)
-    //{
-    //    ScoreboardEntryData newEntryData = new ScoreboardEntryData();
-    //    newEntryData.entryName = username;
-    //    newEntryData.entryScore = 0;
-
-    //    Scoreboard scoreboard = new Scoreboard();
-    //    scoreboard.Initialise(5);
-    //    scoreboard.AddEntry(newEntryData);
-    //}
 }
