85fc2787185c1204e8679db2eaf8b2802347a29f
AlejoBerg <alejo_B@outlook.com>
2021-04-01 16:34:36 -0300

    Graph rotation fix

--- Proyecto Investigacion - UADE/Assets/Scripts/Miscellaneous/RotatorWithClick.cs
+++ Proyecto Investigacion - UADE/Assets/Scripts/Miscellaneous/RotatorWithClick.cs
@@ -11,16 +11,23 @@ public class RotatorWithClick : MonoBehaviour
     [SerializeField] private float _minClampY = -60;
     [SerializeField] private float _maxClampY = 60;
 
+    private void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        float midPoint = (transform.position - Camera.main.transform.position).magnitude * 0.5f;
+        transform.LookAt((ray.origin + ray.direction * midPoint));
+    }
+
     private void OnMouseDrag()
     {
-        float rotX = Input.GetAxis("Mouse X") * Time.deltaTime *_rotationSpeed;
-        float rotY = Input.GetAxis("Mouse Y") * Time.deltaTime * _rotationSpeed;
+        //float rotX = Input.GetAxis("Mouse X") * Time.deltaTime *_rotationSpeed;
+        //float rotY = Input.GetAxis("Mouse Y") * Time.deltaTime * _rotationSpeed;
 
-        Vector3 rot = transform.rotation.eulerAngles + new Vector3(rotY, -rotX, 0f); //use local if your char is not always oriented Vector3.up
-        rot.x = ClampAngle(rot.x, _minClampX, _maxClampX);
-        rot.y = ClampAngle(rot.y, _minClampY, _maxClampY);
+        //Vector3 rot = transform.rotation.eulerAngles + new Vector3(rotY, -rotX, 0f); //use local if your char is not always oriented Vector3.up
+        //rot.x = ClampAngle(rot.x, _minClampX, _maxClampX);
+        //rot.y = ClampAngle(rot.y, _minClampY, _maxClampY);
 
-        transform.eulerAngles = rot;
+        //transform.eulerAngles = rot;
     }
 
     float ClampAngle(float angle, float from, float to)
