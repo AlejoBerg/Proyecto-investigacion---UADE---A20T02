2fc57c2e78c0c774ca6676d5f32f9d4314761b6d
AlejoBerg <alejo_B@outlook.com>
2021-04-02 00:44:23 -0300

    Graph rotation mouse center fix

--- Proyecto Investigacion - UADE/Assets/Scripts/Miscellaneous/RotatorWithClick.cs
+++ Proyecto Investigacion - UADE/Assets/Scripts/Miscellaneous/RotatorWithClick.cs
@@ -4,12 +4,10 @@ using UnityEngine;
 
 public class RotatorWithClick : MonoBehaviour
 {
-    [SerializeField] private float _rotationSpeed = 20;
-
-    [SerializeField] private float _minClampX = -60;
-    [SerializeField] private float _maxClampX = 60;
-    [SerializeField] private float _minClampY = -60;
-    [SerializeField] private float _maxClampY = 60;
+    void OnEnable()
+    {
+        StartCoroutine(ResetMousePosition());
+    }
 
     private void Update()
     {
@@ -18,23 +16,10 @@ public class RotatorWithClick : MonoBehaviour
         transform.LookAt((ray.origin + ray.direction * midPoint));
     }
 
-    private void OnMouseDrag()
+    IEnumerator ResetMousePosition()
     {
-        //float rotX = Input.GetAxis("Mouse X") * Time.deltaTime *_rotationSpeed;
-        //float rotY = Input.GetAxis("Mouse Y") * Time.deltaTime * _rotationSpeed;
-
-        //Vector3 rot = transform.rotation.eulerAngles + new Vector3(rotY, -rotX, 0f); //use local if your char is not always oriented Vector3.up
-        //rot.x = ClampAngle(rot.x, _minClampX, _maxClampX);
-        //rot.y = ClampAngle(rot.y, _minClampY, _maxClampY);
-
-        //transform.eulerAngles = rot;
-    }
-
-    float ClampAngle(float angle, float from, float to)
-    {
-        // accepts e.g. -80, 80
-        if (angle < 0f) angle = 360 + angle;
-        if (angle > 180f) return Mathf.Max(angle, 360 + from);
-        return Mathf.Min(angle, to);
+        Cursor.lockState = CursorLockMode.Locked;
+        yield return new WaitForSeconds(0.1f);
+        Cursor.lockState = CursorLockMode.Confined;
     }
 }
