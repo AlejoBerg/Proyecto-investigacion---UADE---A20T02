57d97720dea36f829886ca06793ebba03ad87e35
AlejoBerg <alejo_B@outlook.com>
2021-07-28 15:39:58 -0300

    Working on help screen

---
+++ Proyecto Investigacion - UADE/Assets/Scripts/HelpScreenScripts/HelpCardsSlider.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelpCardsSlider : MonoBehaviour
+{
+    public HelpGraphButton _currentClickedButton = null;
+    public GameObject _currentCardsParent = null;
+    public List<GameObject> _currentSliderCards = null;
+
+    public void SetNewCardsOnSlider(HelpGraphButton currentClickedButton)
+    {
+        RemoveOldCardsFromSlider();
+
+        _currentClickedButton = currentClickedButton;
+        _currentCardsParent = _currentClickedButton.gameObject;
+        _currentSliderCards = currentClickedButton.InstantiatedCards;
+
+        foreach (var card in _currentSliderCards)
+        {
+            card.transform.parent = this.transform;
+            card.transform.localPosition = new Vector3(this.transform.position.x, this.transform.position.y, 0);
+            card.SetActive(true);
+        }
+    }
+
+    public void RemoveOldCardsFromSlider()
+    {
+        foreach (var card in _currentSliderCards)
+        {
+            card.SetActive(false);
+            card.transform.parent = _currentCardsParent.transform;
+        }
+    }
+}
---
+++ Proyecto Investigacion - UADE/Assets/Scripts/HelpScreenScripts/HelpGraphButton.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelpGraphButton : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> _buttonCardsPrefabs = null;
+    [SerializeField] private HelpCardsSlider _cardsSlider = null;
+
+    private List<GameObject> _instantiatedCards = new List<GameObject>();
+    public List<GameObject> InstantiatedCards { get => _instantiatedCards; set => _instantiatedCards = value; }
+
+    private void Start()
+    {
+        InitButtonCards();
+    }
+
+    private void InitButtonCards()
+    {//Instancio todas las cartas correspondientes a cada boton dentro de la pantalla de help
+        for (int i = 0; i < _buttonCardsPrefabs.Count; i++)
+        {
+            print("Instanciando cartas del boton " + this.gameObject.name);
+
+            var newCard = Instantiate(_buttonCardsPrefabs[i], this.transform.position, Quaternion.identity, this.transform);
+            Destroy(newCard.GetComponent<Draggable>());
+
+            newCard.SetActive(false);
+            _instantiatedCards.Add(newCard);
+        }
+    }
+
+    public void AssignCardsToSlider()
+    {//El slider de la pantalla de help obtiene las instancias de las cartas para mostrar
+
+        print("Pasandole la referencia de las cartas instanciadas al slider");
+
+        _cardsSlider.SetNewCardsOnSlider(this);
+    }
+}
